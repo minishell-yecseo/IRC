b833770f48e66737d1481ebe3d979bafca159245
Wooseok Kim <wooseoki@c3r1s6.42seoul.kr>
2024-01-13 19:56:11 +0900

    Feat: Add ThreadPool class

---
+++ source/ThreadPool.hpp
@@ -0,0 +1,27 @@
+#ifndef THREADPOOL_HPP
+#define THREADPOOL_HPP
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <queue>
+#include "Command.hpp"
+
+class ThreadPool
+{
+	public:
+		std::vector<pthread_t>	threads;
+		std::queue<Command*> 	queue;
+		pthread_mutex_t			lock;
+		pthread_cond_t			notify;
+		int						thread_count;
+		int						count;
+		bool					shutdown;
+			
+		ThreadPool(int size);
+		~ThreadPool();
+
+		void		Enqueue(void *arg);
+		static void	*Worker(void *arg);
+};
+#endif
