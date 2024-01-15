583405fe528cb9d04a9528e94dde15285528b285
SeoSangmin <seoinseoulcity@gmail.com>
2024-01-15 16:05:07 +0900

    Feat : Channel basic class

--- source/Channel.cpp
+++ source/Channel.cpp
@@ -1,1 +1,46 @@
 #include "Channel.hpp"
+
+Channel::Channel() {
+}
+
+bool	Channel::AuthPassword(const std::string& password)
+{
+	if (password.compare(password_) == 0)
+		return true;
+	return false;
+}
+
+bool	Channel::IsMember(const Client& client)
+{
+	std::set<Client>::iterator	it = members_.find(client);
+	if (it != members_.end())
+		return true;
+	return false;
+}
+
+bool	Channel::IsOperator(const Client& client)
+{
+	std::set<Client>::iterator	it = operators_.find(client);
+	if (it != operators_.end())
+		return true;
+	return false;
+}
+
+/* return true when the mode has changed */
+bool	Channel::set_mode(const int& flag, const bool& enable) {
+	if ((mode_ & flag) && !enable)
+	{
+		mode_ &= !flag;
+		return true;
+	}
+	else if (!(mode_ & flag) && enalbe)
+	{
+		mode_ |= flag;
+		return true;
+	}
+	return false;
+}
+
+const char&	Channel::get_mode(void) {
+	return mode_;
+}
--- source/Channel.hpp
+++ source/Channel.hpp
@@ -1,7 +1,36 @@
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
 
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <cstdint>
+#include "Client.hpp"
+
+#define MODE_INVITE (1 << 2)
+#define MODE_TOPIC	(1 << 1)
+#define MODE_KEY	(1)
+
 class Channel {
+	private:
+		std::string	name_;
+		std::string	password_;
+		std::string	topic_;
+		char	mode_;
+	
+		std::set<Client>	members_;
+		std::set<Client>	operators_;
+		std::set<Client>	limits_;
+	
+	public:
+		bool	IsMember(const Client& client);
+		bool	IsOperator(const Client& client);
+		bool	AuthPassword(const std::string& pw);
+
+	public:
+		bool	set_mode(const int& flag, const bool& enable);
+		const char&	get_mode(void);
 };
 
 #endif
