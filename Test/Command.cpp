3f3a5b3bb7164b20a9add98f6160be8a48165d51
Wooseok Kim <wooseoki@c3r1s2.42seoul.kr>
2024-01-15 13:45:56 +0900

    Test: For threadpool

---
+++ Test/Command.cpp
@@ -0,0 +1,33 @@
+#include "Command.hpp"
+
+#include <vector>
+
+Command::Command(const std::vector<std::string> &token_list)
+{
+	token_list.size();
+	//std::cout << token_list.length() << "Need init of command\n";
+
+	/*
+	if (token_list[0][0] == ':')
+	{
+		this->prefix_ = token_list[0];
+		this->command_ = token_list[1];
+	}
+	else
+	{
+		this->command_ = token_list[0];
+		this->params_ = &token_list[1];
+	}
+	*/
+}
+
+CapCommand::CapCommand(const std::vector<std::string> &token_list) : Command(token_list)
+{
+	;
+}
+
+void	CapCommand::Run()
+{
+
+	;
+}
