a4c8fcfd013d4fd784272350f72dcaa13f35bd35
SeoSangmin <seoinseoulcity@gmail.com>
2024-01-16 13:20:16 +0900

    Test : leak test of main

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,15 @@
 #include "ircserv.h"
 
+void leaks();
 int main(int argc, char **argv)
 {
+	atexit(leaks);
 	Server server(argc, argv);
 	server.run();
 	return 0;
+}
+
+void	leaks()
+{
+	system("leaks $PPID");
 }
