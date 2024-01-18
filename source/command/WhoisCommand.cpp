212fe64316928fa9c0d07f1a2ce5912f8dcf3d64
Wooseok Kim <wooseoki@c3r1s3.42seoul.kr>
2024-01-18 15:35:05 +0900

    Feat: Add WhoisCommand

--- source/command/WhoisCommand.cpp
+++ source/command/WhoisCommand.cpp
@@ -3,6 +3,31 @@
 WhoisCommand::WhoisCommand(const std::vector<std::string> &token_list) : Command(token_list) {
 }
 
+/*
+ *ERR_NOSUCHNICK (401)
+ *ERR_NOSUCHSERVER (402)
+ *ERR_NONICKNAMEGIVEN (431)
+ *RPL_ENDOFWHOIS (318)
+*/
+
+/*
+ * This section describes extension to the common WHOIS command above.
+ * They exist mainly on historical servers, and are rarely implemented,
+ * because of resource usage they incur.
+ */
+
+// We just reply ENDOFWHOIS or such ERR
 void	WhoisCommand::Run() {
-	/**/
+	if (this->params_.empty())
+		//ERR_NONICKNAMEGIVEN;
+		;
+	if (find(this->params_[0]) == end())
+		//ERR_NOSUCHNICK;
+		;
+	
+	std::string message = "318 WHOISEND\r\n";
+	server_->pool_->LockClientMutex(client_sock_);
+	send(client_->get_sock(), message.c_str(), message.size(), 0);
+	server_->pool_->UnlockClientMutex(client_sock_);
+
 }
