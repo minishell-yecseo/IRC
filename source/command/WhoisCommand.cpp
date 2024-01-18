6d79fdeaaaebca6b502ddf2325a0b32586c4b49b
Wooseok Kim <wooseoki@c3r1s3.42seoul.kr>
2024-01-18 16:01:49 +0900

    Feat: WhoisCommand, PingCommand add BNF

--- source/command/WhoisCommand.cpp
+++ source/command/WhoisCommand.cpp
@@ -8,6 +8,7 @@ WhoisCommand::WhoisCommand(const std::vector<std::string> &token_list) : Command
  *ERR_NOSUCHSERVER (402)
  *ERR_NONICKNAMEGIVEN (431)
  *RPL_ENDOFWHOIS (318)
+ *ERR_NOTREGISTERED (451)
 */
 
 /*
@@ -18,13 +19,15 @@ WhoisCommand::WhoisCommand(const std::vector<std::string> &token_list) : Command
 
 // We just reply ENDOFWHOIS or such ERR
 void	WhoisCommand::Run() {
+	// Need function in mutex
+	/*
+	if (this->client_->auth_ == false)
+		; //ERR_NOTREGISTERD;
 	if (this->params_.empty())
-		//ERR_NONICKNAMEGIVEN;
-		;
+		; //ERR_NONICKNAMEGIVEN;
 	if (find(this->params_[0]) == end())
-		//ERR_NOSUCHNICK;
-		;
-	
+		; //ERR_NOSUCHNICK;
+	*/
 	std::string message = "318 WHOISEND\r\n";
 	server_->pool_->LockClientMutex(client_sock_);
 	send(client_->get_sock(), message.c_str(), message.size(), 0);
