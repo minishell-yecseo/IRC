2f90e83d239852f1d9ac681b9ec61cc53e5c2ceb
SeoSangmin <seoinseoulcity@gmail.com>
2024-01-18 10:24:33 +0900

    Refactor : write -> send modify

--- source/command/CapCommand.cpp
+++ source/command/CapCommand.cpp
@@ -7,7 +7,8 @@ void	CapCommand::Run() {
 	/**/
 	std::string message;
 	message = "CAP * LS :\r\n";
-	server_->pool_->LockClientMutex(client_sock_);
-	write(client_->get_sock(), message.c_str(), message.size());
-	server_->pool_->UnlockClientMutex(client_sock_);
+
+	server_->pool_->LockClientMutex(client_sock_);//Client Lock
+	send(client_->get_sock(), message.c_str(), message.size(), 0);
+	server_->pool_->UnlockClientMutex(client_sock_);//Client Unlock
 }
