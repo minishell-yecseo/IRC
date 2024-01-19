5cb7b66283c0b7e58fda270a9762a3145dd47c42
Wooseok Kim <wooseoki@c3r1s2.42seoul.kr>
2024-01-19 17:15:11 +0900

    Delete: Unused server name

--- source/command/PingCommand.cpp
+++ source/command/PingCommand.cpp
@@ -14,7 +14,8 @@ void	PingCommand::Run() {
 		; //ERR_NEEDMOREPARAMS;
 	*/
 	// Need get_server_name function
-	std::string	message = "PONG HAPPY " + this->params_[0] + "\r\n";
+	std::string	message = "PONG " + this->params_[0] + "\r\n";
+	log::cout << message;
 	server_->pool_->LockClientMutex(client_sock_);
 	send(client_->get_sock(), message.c_str(), message.size(), 0);
 	server_->pool_->UnlockClientMutex(client_sock_);
