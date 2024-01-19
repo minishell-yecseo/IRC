81ce217b5e62ef4bc6ddb67343e5c252d7547128
Wooseok Kim <wooseoki@c3r1s2.42seoul.kr>
2024-01-19 12:48:04 +0900

    Feat: Add MakeFormat function for response message

--- source/Response.cpp
+++ source/Response.cpp
@@ -1,1 +1,21 @@
 #include "Response.hpp"
+
+std::string	MakeFormat(const std::string& prefix, const std::string& numeric_reply, const std::vector<std::string>& params)
+{
+	std::string	response;
+
+	if	(prefix.empty() == false) {
+		response += prefix + " "
+	}
+	if (numeric_reply.empty() == false) {
+		response += numeric_reply + " "
+	}
+	for (size_t i = 0; i < params.size(); ++i) {
+		response += params[i];
+		if (i < params.size() - 1){
+			response += " "
+		}
+	}
+	response += CRLF;
+	return response.str();
+}
