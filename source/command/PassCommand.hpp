b421c248a931e1434ef99eb3791e99bb23632771
SeoSangmin <seoinseoulcity@gmail.com>
2024-01-22 14:18:41 +0900

    Feat : Pass with already authenticated client

--- source/command/PassCommand.hpp
+++ source/command/PassCommand.hpp
@@ -8,5 +8,6 @@ class PassCommand: public Command {
 		PassCommand(const std::vector<std::string> &token_list);
 		void	Run(void);
 		std::string	AnyOfError(void);
+		bool		CheckClientAuth(void);
 };
 #endif
