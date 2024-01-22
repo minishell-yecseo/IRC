175a50038820a17eb3037af0a9644ffc811a016c
SeoSangmin <seoinseoulcity@gmail.com>
2024-01-22 09:51:02 +0900

    Fix : Command 001 response CRLF error

--- source/Command.cpp
+++ source/Command.cpp
@@ -67,6 +67,7 @@ void	Command::AuthCheckReply(void) {
 	this->server_->UnlockClientMutex(this->client_sock_);//Unlock
 	
 	if (auth_status == true) {
+		auth_message << CRLF;
 		SendResponse(this->client_sock_, auth_message.get_str());
 		log::cout << RED << "send 001\n" << RESET;
 	}
