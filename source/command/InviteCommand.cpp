38dd3fcba1abf355b93dee4b6645aed041ffc8a3
Wooseok Kim <wooseoki@c3r1s4.42seoul.kr>
2024-01-22 12:50:46 +0900

    Feat: Implement Invite command

--- source/command/InviteCommand.cpp
+++ source/command/InviteCommand.cpp
@@ -3,29 +3,27 @@
 InviteCommand::InviteCommand(const std::vector<std::string> &token_list) : Command(token_list) {
 }
 
-/*
-RPL_INVITING (341)
-ERR_NOSUCHNICK (401)
-ERR_NEEDMOREPARAMS (461)
-ERR_NOSUCHCHANNEL (403)
-ERR_NOTONCHANNEL (442)
-ERR_CHANOPRIVSNEEDED (482)
-ERR_USERONCHANNEL (443)
-*/
-
 std::string	InviteCommand::CheckChannel(const std::string& nick, const std::string& chan) {
 	std::string	dummy;
 
 	int	receiver = this->server_->SearchClientByNick(nick);
+	int sender = 0;
 	int	ret = 0;
 
+	sender = this->client_sock_;
 	if (receiver == FT_INIT_CLIENT_FD)
-		return dummy + ERR_NOSUCHNICK + " " + nick + " Invite :No Such User";
+		return dummy + ERR_NOSUCHNICK + " " + nick + " :No such user";
 	pthread_mutex_lock(&this->server_->pool_->s_channels_mutex_);
-	ret = this->server_->CheckInviteError(chan, this->client_sock_, receiver);
+	ret = this->server_->CheckInviteError(chan, sender, receiver);
 	pthread_mutex_unlock(&this->server_->pool_->s_channels_mutex_);
-	if (ret == 0)
-		return "";
+	if (ret == CHANNOTFOUND)
+		return dummy + ERR_NOSUCHCHANNEL + " " + chan + " :No such channel";
+	if (ret == SENDERNOTFOUND)
+		return dummy + ERR_NOTONCHANNEL + " " + chan + " :You're not on that channel";
+	if (ret == NOTOPERATOR)
+		return dummy + ERR_CHANOPRIVSNEEDED + " " + chan + " :You're not channel operator";
+	if (ret == ALREADYIN)
+		return dummy + ERR_USERONCHANNEL + " " + nick +" " + chan + " :is already on cahnnel";
 	return dummy;
 }
 
@@ -41,8 +39,10 @@ void	InviteCommand::Run() {
 	Response	r;
 
 	r << AnyOfError();
-	if (r.size() > 0)
+	if (r.IsError() == true)
 		return SendResponse(this->client_sock_, r.get_format_str());
 	//<client> <nick> <channel>
+	r << RPL_INVITING << " " << this->params_[0] << " " << this->params_[1];
+	SendResponse(this->client_sock_, r.get_format_str());
 }
 
--- source/command/InviteCommand.hpp
+++ source/command/InviteCommand.hpp
@@ -3,6 +3,21 @@
 
 #include "Command.hpp"
 
+#define CHANNOTFOUND 	1
+#define SENDERNOTFOUND 	2
+#define NOTOPERATOR		3
+#define	ALREADYIN		4
+
+/*
+RPL_INVITING (341)
+ERR_NOSUCHNICK (401)
+ERR_NEEDMOREPARAMS (461)
+ERR_NOSUCHCHANNEL (403)
+ERR_NOTONCHANNEL (442)
+ERR_CHANOPRIVSNEEDED (482)
+ERR_USERONCHANNEL (443)
+*/
+
 class InviteCommand: public Command {
 	public:
 		InviteCommand(const std::vector<std::string> &token_list);
