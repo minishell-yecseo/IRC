999f26c95f690432db4b1444cc8261f63552bca5
SeoSangmin <seoinseoulcity@gmail.com>
2024-01-22 11:51:36 +0900

    Feat : Mutex class create

---
+++ source/Mutex.hpp
@@ -0,0 +1,20 @@
+#ifndef MUTEX_HPP
+# define MUTEX_HPP
+
+#include <pthread.h>
+
+class	Mutex {
+	public:
+		Mutex(void);
+		Mutex(const pthread_mutexattr_t *attr);
+		~Mutex(void);
+		int	lock(void);
+		int	unlock(void);
+		int	init(const pthread_mutexattr_t *attr = NULL);
+
+	private:
+		pthread_mutex_t	mutex_;
+		int	destroy(void);
+};
+
+#endif
