0511a143b4f7aabba4e8202a8344b5b2298c89a4
Wooseok Kim <wooseoki@c3r1s2.42seoul.kr>
2024-02-08 13:15:50 +0900

    Refactor: Seperate Command test

---
+++ Test/main.cpp
@@ -0,0 +1,33 @@
+#include "TestRequest.hpp"
+
+Server	*ServerInit(void) {
+	char *argv[] = {"TEST", "9090", "1234"};
+	Server *result;
+
+	result = new Server(3, argv);
+	return result;
+}
+
+Client*	ClientInit(void) {
+	Client *result;
+
+	result = new Client;
+	result->set_sock(9);
+	result->set_nick("wooseoki");
+	return result;
+}
+
+
+Mutex print;
+int main() {
+	Server *s;
+	Client *c;
+
+	print.init(NULL);
+	s = ServerInit();
+	c = ClientInit();
+	TestResponse(s, c);
+
+	delete s;
+	delete c;
+}
