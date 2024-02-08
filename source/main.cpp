3cbf07e936fef2044d72df62d54699c6b0add350
Wooseok Kim <wooseoki@c3r1s2.42seoul.kr>
2024-02-08 16:45:48 +0900

    Feat: Delete unused function

--- source/main.cpp
+++ source/main.cpp
@@ -4,17 +4,11 @@
 
 Mutex print;
 
-void leaks();
 int main(int argc, char **argv) {
-//	atexit(leaks);
 	print.init(NULL);
 
 	Server server(argc, argv);
 	server.Run();
 
 	return 0;
-}
-
-void	leaks() {
-	system("leaks --list -- $PPID");
 }
