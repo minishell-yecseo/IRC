38b3e8a730949d90c4a346399d9e66ac7ec37c16
SeoSangmin <seoinseoulcity@gmail.com>
2024-02-21 10:39:18 +0900

    Feat : Send Quit messages to channel shared clients

--- source/command/QuitCommand.cpp
+++ source/command/QuitCommand.cpp
@@ -21,25 +21,34 @@ void	QuitCommand::Run(void) {
 
 void	QuitCommand::NoticeQuit(void) {
 	this->resp_ = this->sender_nick_ + " QUIT :";
-	if (this->params_.size() > 1)
-		this->resp_ << params_[1];
+	if (this->params_.size() > 0)
+		this->resp_ << params_[0];
 
 	this->server_->LockClientMutex(this->client_sock_);
+	std::set<int> sent_client;
 	std::vector<std::string> channels = this->client_->get_channels();
 	std::vector<std::string>::iterator channel_itr = channels.begin();
 	Channel *channel_ptr = NULL;
 	while (channel_itr != channels.end()) {
 		channel_ptr = this->server_->get_channel_ptr(*channel_itr);
 		if (channel_ptr != NULL) {
-			if (this->server_->LockChannelMutex(*channel_itr)) {
+			try {
+				this->server_->LockChannelMutex(*channel_itr);
 				std::map<int, char> mem = channel_ptr->get_members();
 				std::map<int, char>::iterator member_itr = mem.begin();
 				while (member_itr != mem.end()) {
-					SendResponse(member_itr->first, this->resp_.get_format_str());
+					if (member_itr->first != this->client_sock_ && \
+					sent_client.find(member_itr->first) == sent_client.end()) {
+						SendResponse(member_itr->first, this->resp_.get_format_str());
+						sent_client.insert(member_itr->first);
+					}
 					member_itr++;
 				}
+				this->server_->UnlockChannelMutex(*channel_itr);
+			} catch (std::exception& e) {
+				this->server_->UnlockClientMutex(this->client_sock_);
+				throw(e);
 			}
-			this->server_->UnlockChannelMutex(*channel_itr);
 		}
 		channel_itr++;
 	}
